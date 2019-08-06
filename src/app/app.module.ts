a2b8e23d5318ebf67766fdb69c98ea4890d7f0a4
Divyanshu <divyanshu.r46956@gmail.com>
2019-08-06 13:50:08 +0200

    Code refactoring.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DrawingCanvasComponent } from './Components/drawing-canvas/drawing-canvas.component';
-import { CanvasComponent } from './Components/canvas/canvas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DrawingCanvasComponent,
-    CanvasComponent
   ],
   imports: [
     BrowserModule
